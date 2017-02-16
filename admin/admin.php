098f4afbf3e476761a0178681f9a93be01033233
goldgshok <goldgshok@gmail.com>
2017-02-16 13:30:19 +0700

    - подготовка админки для сайта

---
+++ admin/admin.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Тестируем PHP</title>
+  </head>
+<body>
+
+<?php
+  require_once 'connection.php';
+
+  // подключаемся к серверу
+  $link = mysqli_connect($host, $user, $password, $database) 
+    or die("Ошибка " . mysqli_error($link));
+ 
+  // выполняем операции с базой данных
+  $query ="SELECT * FROM price_type";
+  $result = mysqli_query($link, $query) 
+    or die("Ошибка " . mysqli_error($link)); 
+  if($result)  
+  {
+    echo "Выполнение запроса прошло успешно";
+  }  
+   
+  // закрываем подключение
+  mysqli_close($link);
+?>
+</body>
+</html>
---
+++ admin/connection.php
@@ -0,0 +1,6 @@
+<?php
+  $host = 'eu-cdbr-azure-north-e.cloudapp.net';
+  $dbname = 'acsm_d4a0065602d66b1';
+  $user = 'bf020a4db22631';
+  $pass = '2acecacf';
+?>
