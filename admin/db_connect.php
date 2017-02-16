eb61da3800df1465fd7276e5938ad702fcb236fb
goldgshok <goldgshok@gmail.com>
2017-02-16 17:21:55 +0700

    - ошибка видимости

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -8,30 +8,30 @@ class DB_CONNECT
     // import database connection variables
     require_once 'config.php';
 
-    $mysqli = new mysqli($host, $user, $pass, $dbname);
+    $this->$mysqli = new mysqli($host, $user, $pass, $dbname);
 
-    if ($mysqli->connect_errno)  
+    if ($this->$mysqli->connect_errno)  
     {
-      echo "Ошибка: " . $mysqli->connect_error . "\n";
+      echo "Ошибка: " . $this->$mysqli->connect_error . "\n";
       exit;
     }
     
-    if (!$mysqli->set_charset($charset)) 
+    if (!$this->$mysqli->set_charset($charset)) 
     {
-      printf("Ошибка при загрузке набора символов $charset: %s\n", $mysqli->error);
+      printf("Ошибка при загрузке набора символов $charset: %s\n", $this->$mysqli->error);
       exit;
     } 
     else 
     {
-      printf("Текущий набор символов: %s\n", $mysqli->character_set_name());
+      printf("Текущий набор символов: %s\n", $this->$mysqli->character_set_name());
     }
   }
 
   public function getResult($sql)
   {
-    if (!$result = $mysqli->query($sql))
+    if (!$result = $this->$mysqli->query($sql))
     {
-      echo "Ошибка: " . $mysqli->error . "\n";
+      echo "Ошибка: " . $this->$mysqli->error . "\n";
       exit;
     }
     return $result;
@@ -39,7 +39,7 @@ class DB_CONNECT
 
   public function close() 
   {
-    $mysqli->close();
+    $this->$mysqli->close();
   }
 }
 ?>
