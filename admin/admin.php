50b2b6769120573e653e66824ccc932fc6b113bd
goldgshok <goldgshok@gmail.com>
2017-02-16 13:43:33 +0700

    - пробуем вывести первую инфу из базы

--- admin/admin.php
+++ admin/admin.php
@@ -12,13 +12,20 @@
     or die("Ошибка " . mysqli_error($link));
  
   // выполняем операции с базой данных
-  $query ="SELECT * FROM price_type";
+  $query ="SELECT ID, Name FROM price_type";
   $result = mysqli_query($link, $query) 
     or die("Ошибка " . mysqli_error($link)); 
-  if($result)  
+
+  print "<table>\n";
+  while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) 
   {
-    echo "Выполнение запроса прошло успешно";
-  }  
+    print "\t<tr>\n";
+    foreach ($line as $col_value) {
+      print "\t\t<td>$col_value</td>\n";
+    }
+    print "\t</tr>\n";
+  }
+  print "</table>\n"; 
    
   // закрываем подключение
   mysqli_close($link);
