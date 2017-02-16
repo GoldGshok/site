836a3c84bd07b66ef322529bf74eb084df490a64
goldgshok <goldgshok@gmail.com>
2017-02-16 14:21:26 +0700

    - немного передеалал структуру

--- admin/admin.php
+++ admin/admin.php
@@ -5,17 +5,16 @@
 <body>
 
 <?php
-  require_once 'connection.php';
   require_once 'console.php';
+  requere_once 'db_connect.php';
 
-  // подключаемся к серверу
-  $link = mysqli_connect($host, $user, $pass, $dbname) 
-    or die("Ошибка " . mysqli_error($link));
+  $db = new DB_CONNECT();
+  $db->connect();
  
   // выполняем операции с базой данных
   $query = "SELECT pt.ID, pt.Name FROM price_type pt";
-  $result = mysqli_query($query) 
-    or die(mysqli_error()); 
+  $result = $db->query($query);  
+
   console_log($result->num_rows);
  
   print "<table>\n";
@@ -23,10 +22,7 @@
     printf("<tr><td>%s</td><td>%s</td></tr>", $row["ID"], $row["Name"]);
   }
   print "</table>\n"; 
-   
-  $result->close();
-  // закрываем подключение
-  mysqli_close($link);
+
 ?>
 </body>
 </html>
---
+++ admin/db_connect.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class DB_CONNECT 
+{
+  function connect() 
+  {
+    // import database connection variables
+    require_once 'connection.php';
+    $connection = mysql_connect($host, $user, $pass) or die(mysql_error());
+
+    // Selecing database
+    mysql_select_db($dbname, $connection) or die(mysql_error());
+  }
+  
+  function query($query) {
+    $result = mysql_query($query) or die(mysql_error());
+    return $result;        
+  } 
+  
+  function close() {
+    // closing db connection
+    mysql_close();
+  }
+}
+?>
