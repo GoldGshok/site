eca37ac6f96a4326a3dd1c3da6f1b18e1d80c7c5
GoldGshok <goldgshok@gmail.com>
2017-02-17 23:48:00 +0700

    - заказы

--- admin/orders.php
+++ admin/orders.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>Админка</title>
+    <title>Заказы</title>
     <link href="../styles/styles.css" rel="stylesheet">
   </head>
 <body>
@@ -19,7 +19,10 @@
   $db->connect();
 
   // выполняем операции с базой данных
-  $sql = 'SELECT ID, Name FROM price_type';
+  $sql = 'SELECT o.ID, i.Name AS Item, cl.Name, p.Cost, o.Date  FROM orders o
+    INNER JOIN clients cl ON cl.ID = o.ID_client
+    INNER JOIN items i ON i.ID = o.ID_item
+    INNER JOIN prices p ON p.ID = i.ID_price_sell AND p.ID_price_type = 1';
 
   $result = $db->getResult($sql);
   
@@ -32,11 +35,14 @@
   print '<table>';
   print '<tr>
       <th>ID</th>
-      <th>Тип цены</th>
+      <th>Название картины</th>
+      <th>Имя клиента</th>
+      <th>Стоимость продажи</th>
+      <th>Дата создания</th>
     </tr>';
   while ($row = $result->fetch_assoc()) 
   {
-    printf("<tr><td>%s</td> <td>%s</td></tr>", $row["ID"], $row["Name"]);
+    printf("<tr><td>%s</td> <td>%s</td></tr>", $row["ID"], $row["Item"], $row["Name"], $row["Cost"], $row["Date"]);
   }
   print '</table>';
 
