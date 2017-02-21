f02340693a0ac48ce97ec1181aa1e42c68c73886
goldgshok <goldgshok@gmail.com>
2017-02-21 16:16:41 +0700

    - небольшие исправления

---
+++ admin/authorize.php
@@ -0,0 +1,39 @@
+<?php
+  // открываем сессию
+  session_start();
+  
+  $SERVER_ROOT = "http://paintingbynumbers.azurewebsites.net/admin/";
+  
+  require_once ('db_connect.php');
+  
+  if (preg_match("/^$SERVER_ROOT/",$_SERVER['HTTP_REFERER']))
+  {
+    // данные были отправлены формой?
+    if ($_POST['Submit'])
+    {
+      $db = new DB_CONNECT();
+      $db->connect();
+    
+      $user = $_POST['user_name'];
+      $password = md5($_POST['user_pass']);
+    
+      $sql = "
+        SELECT 1 
+        FROM users u 
+        WHERE u.Login = '$user' 
+          AND u.Password = '$password'";
+    
+      $result = $db->getResult($sql);
+       
+      if ($result->num_rows > 0)
+      {
+        // запоминаем имя пользователя
+        $_SESSION['logged_user'] = $_POST['user_name'];
+        // перенаправляем
+        header("Location: orders.php");
+        exit;
+      }
+   
+    }
+  }
+?>
