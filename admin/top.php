ce19dcfc70410fc79677c2489be60978670d9360
goldgshok <goldgshok@gmail.com>
2017-02-21 16:30:53 +0700

    - выход из админки

--- admin/top.php
+++ admin/top.php
@@ -5,6 +5,7 @@
 <li><a href="orders.php">Заказы</a></li>
 <li><a href="catalog.php">Каталог</a></li>
 <li><a href="clients.php">Клиенты</a></li>
+<li><a href="disconnect.php">Выход</a></li>
 </ul>
 </nav>
 
