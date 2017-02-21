ec5384e5ea57e279df680fc857fcf16c3307f982
goldgshok <goldgshok@gmail.com>
2017-02-21 17:53:50 +0700

    - добавлены новые страницы

---
+++ admin/authors.php
@@ -0,0 +1,86 @@
+<html>
+  <head>
+    <title>Авторы</title>
+    <link href="../styles/styles.css" rel="stylesheet">
+  </head>
+<body>
+
+  <div class="head">
+    <?php
+      //очищаем адресную строку
+      unset($_SESSION['logged_user']);
+      //открываем сессию
+      session_start();
+      
+      if (!isset($_SESSION['logged_user']))
+      {
+        header("Location: admin.php");
+        exit;
+      }
+      
+      require_once ('top.php'); 
+    ?>
+  </div>
+
+<?php
+  require_once ('db_connect.php');
+
+  $db = new DB_CONNECT();
+  $db->connect();
+
+  // выполняем операции с базой данных
+  $sql = '';
+
+  $result = $db->getResult($sql);
+  
+  if ($result->num_rows == 0) 
+  {
+    echo "Извините, данные не найдены.";
+    exit;
+  }  
+ 
+  print '<table>';
+  print '<tr>
+      <th>ID</th>
+      <th>Артикул</th>
+      <th>Наименование</th>
+      <th>Изображение</th>
+      <th>Автор</th>
+      <th>Размер</th>
+      <th>Тип</th>
+      <th>Сложность</th>
+      <th>Стоимость покупки (руб.)</th>
+      <th>Стоимость продажи (руб.)</th>
+    </tr>';
+  while ($row = $result->fetch_assoc()) 
+  {
+    printf("<tr>
+      <td>%s</td> 
+      <td>%s</td>
+      <td>%s</td>
+      <td><img src='%s' width='200' height='200'/></td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      </tr>", 
+      $row["ID"], 
+      $row["Article"], 
+      $row["Item"], 
+      $row["Site"], 
+      $row["Author"], 
+      $row["Size"], 
+      $row["Type"], 
+      $row["Complexity"],
+      $row["Buy_Cost"],
+      $row["Sell_Cost"]);
+  }
+  print '</table>';
+
+  $result->close();
+
+?>
+</body>
+</html>
--- admin/top.php
+++ admin/top.php
@@ -5,6 +5,7 @@
 <li><a href="orders.php">Заказы</a></li>
 <li><a href="catalog.php">Каталог</a></li>
 <li><a href="clients.php">Клиенты</a></li>
+<li><a href="authors.php">Авторы</a></li>
 <li><a href="exit.php">Выход</a></li>
 </ul>
 </nav>
