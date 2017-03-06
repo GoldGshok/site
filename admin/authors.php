34e2960a232246f287170a8168ab77a7bbaa3937
goldgshok <goldgshok@gmail.com>
2017-03-06 16:27:51 +0700

    -

--- admin/authors.php
+++ admin/authors.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <title>Авторы</title>
-    
+    <link href="../styles/styles.css" rel="stylesheet">
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.min.js">
     </script>
